157cf4222bf459f5381c938a5cfe2bcf7a822f9c
Scott Gilbert <gilbert.scott0616@gmail.com>
2020-03-02 11:10:28 -0600

    final kitchen sink

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const states = 50;
 let add = 5 + 4
 
 
-console.log(add);
-
 //Added 5+4, checked answer in console.log
 
 
@@ -29,6 +27,8 @@ checkAge('Charles', 21);
 checkAge('Abbey', 27)
 checkAge('James', 18)
 checkAge('John', 17)
+
+//Created a function to check the age of people, and make sure they were 21 before entering the webpage
 
 let friends = [
     {
@@ -54,3 +54,42 @@ let veggies = ['Carrots', 'Peppers', 'Peas', 'Asparagus']
 for (let i = 0; i < veggies.length; i++) {
     console.log(veggies[i]);
 }
+
+
+let people  = [
+    {
+        name: 'Bob',
+        age: 14
+    },
+    {
+        name: 'Will',
+        age: 50
+    },
+    {
+        name: 'Sam',
+        age: 25
+    },
+    {
+        name: 'Holly',
+        age: 27
+    },
+    {
+        name: 'Nick',
+        age: 18
+    },
+]
+
+for (let i = 0; i < people.length; i++) {
+    checkAge(people[i].name, people[i].age);
+}
+
+
+function getLength(word) {
+    if (word.length % 2 === 0) {
+        console.log('The world is nice and even!');
+    }
+        else {
+            console.log('The world is an odd place!');
+        }
+}
+getLength("Hello World");
